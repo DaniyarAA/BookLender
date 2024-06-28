8049bedbdff9ec48392f01091276f356c2be96ee
DaniyarM. <gitmailhub@gmail.com>
2024-06-28 16:49:38 +0600

    Initial commit

---
+++ hw43/src/Main.java
@@ -0,0 +1,15 @@
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            HttpServer server = Server.makeServer();
+            Server.initRoutes(server);
+            server.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
